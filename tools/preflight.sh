efc00c52a5863de38df633f5d62e2c7d8aa4c438
Florian Hübner <code@hub.lol>
2023-07-11 20:28:40 +0200

    go version

--- tools/preflight.sh
+++ tools/preflight.sh
@@ -9,9 +9,9 @@ if ! v=$(go version | grep -E -o "go1\.[^.]+"); then
     exit 1
 fi
 
-if test "$v" \< "go1.16"; then
+if test "$v" \< "go1.20"; then
     echo >&2 "Go version is too old: ${v}"
-    echo >&2 "Go 1.16+ is required"
+    echo >&2 "Go 1.20+ is required"
     exit 1
 fi
 
