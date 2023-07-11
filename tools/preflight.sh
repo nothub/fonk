64a920963fc8b0f221fc20d2c5add51d72c2a633
Florian Hübner <code@hub.lol>
2023-07-11 18:23:41 +0200

    fix fd

--- tools/preflight.sh
+++ tools/preflight.sh
@@ -5,19 +5,19 @@ set -eu
 cd "$(realpath "$(dirname "$(readlink -f "$0")")/..")"
 
 if ! v=$(go version | grep -E -o "go1\.[^.]+"); then
-    echo >&2"Failed to identify go version"
+    echo >&2 "Failed to identify go version"
     exit 1
 fi
 
 if test "$v" \< "go1.16"; then
-    echo >&2"Go version is too old: ${v}"
-    echo >&2"Go 1.16+ is required"
+    echo >&2 "Go version is too old: ${v}"
+    echo >&2 "Go 1.16+ is required"
     exit 1
 fi
 
 if test \! \( -e /usr/include/sqlite3.h -o -e /usr/local/include/sqlite3.h \); then
-    echo >&2"Unable to find sqlite3.h header"
-    echo >&2"Please install libsqlite3 dev package"
+    echo >&2 "Unable to find sqlite3.h header"
+    echo >&2 "Please install libsqlite3 dev package"
     exit 1
 fi
 
